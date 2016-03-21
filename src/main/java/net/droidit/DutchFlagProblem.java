f973e468e1c1ddae78ca7a9161d1389cb33ca9f5
Rumesh <p.rumesh@outlook.com>
2016-03-21 02:29:04 +0000

    Added Dutch flag problem

---
+++ src/main/java/net/droidit/DutchFlagProblem.java
@@ -0,0 +1,36 @@
+package net.droidit;
+
+public class DutchFlagProblem
+{
+    private int[] array;
+    private int pivotIndex = 0;
+    
+    public DutchFlagProblem(int[] array, int pivotIndex)
+    {
+        this.array = array;
+        this.pivotIndex = pivotIndex;
+    }
+    
+    public void dutchFlagPartition()
+    {
+        int i = 0;
+        int n = array.length;
+        int pivot = array[pivotIndex];
+        int element = array[i];
+        int lastIndex = n;
+        int firstIndex = 0;
+        
+        while(i < n)
+        {
+            if(element < pivot)
+            {
+                ArrayUtils.swap(array, i, firstIndex++);
+            }
+            else if(element > pivot)
+            {
+                ArrayUtils.swap(array, i, lastIndex--);
+            }
+            i++;
+        }
+    }
+}
